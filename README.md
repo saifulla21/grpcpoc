15cb5e2115d9bef1f8d8d00d1dd591af84a4bd9d
Saifulla Shaik <shaiksaifulla21@gmail.com>
2018-05-10 17:53:55 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# grpcpoc
+[This code's documentation lives on the grpc.io site.](https://grpc.io/docs/quickstart/python.html)
---
+++ greeter_server.py
@@ -0,0 +1,67 @@
+# Copyright 2015 gRPC authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""The Python implementation of the GRPC helloworld.Greeter server."""
+
+from concurrent import futures
+import time
+
+import grpc
+
+import helloworld_pb2
+import helloworld_pb2_grpc
+
+from random import randint
+from threading import current_thread
+from collections import defaultdict
+
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
+
+class Greeter(helloworld_pb2_grpc.GreeterServicer):
+
+    def SayHello(self, request, context):
+        response = 'test'
+        print ('responding')
+        return helloworld_pb2.HelloReply(message='Hello, %s!' % request.name, response=response)
+
+    def SayHelloAgain(self, request, context):
+        return helloworld_pb2.HelloReply(message='Hello, %s!' % request.name)
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    try:
+        while True:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+    except KeyboardInterrupt:
+        server.stop(0)
+
+def intense_cpu():
+    x = 1
+    threshold = randint(1, 10)
+    print ('threshold value %s' % str(threshold))
+    start = int(time.time())
+    while True:
+        delta = int(time.time()) - start
+        if (delta > threshold):
+            break
+        x= x*x
+    return 'threshold : '+str(threshold)
+
+
+if __name__ == '__main__':
+    serve()
